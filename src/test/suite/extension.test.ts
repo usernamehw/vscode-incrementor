97063fda6b1839d04e0fd7494c83c73ebeef5aa8
al <usernamehw@gmail.com>
2020-04-22 11:44:22 +0300

    Add test for custom value

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -8,7 +8,7 @@ import { Incrementor } from '../../extension';
 const DELAY_VALUE = 100;
 const editor = vscode.window.activeTextEditor;
 const { document } = editor;
-// const inc = new Incrementor();
+const incrementor = new Incrementor();
 
 const delay = async (ms: number): Promise<void> => new Promise(resolve => setTimeout(resolve, ms));
 
@@ -27,24 +27,29 @@ async function getAllEditorText(): Promise<string> {
 	await delay(DELAY_VALUE);
 	return document.getText();
 }
-async function runIncrement(): Promise<void> {
+
+async function inc(): Promise<void> {
 	await vscode.commands.executeCommand('incrementor.incByOne');
 }
-async function runIncrement10(): Promise<void> {
+async function inc10(): Promise<void> {
 	await vscode.commands.executeCommand('incrementor.incByTen');
 }
-async function runIncrement01(): Promise<void> {
+async function inc01(): Promise<void> {
 	await vscode.commands.executeCommand('incrementor.incByTenth');
 }
-async function runDecrement(): Promise<void> {
+async function dec(): Promise<void> {
 	await vscode.commands.executeCommand('incrementor.decByOne');
 }
-async function runDecrement10(): Promise<void> {
+async function dec10(): Promise<void> {
 	await vscode.commands.executeCommand('incrementor.decByTen');
 }
-async function runDecrement01(): Promise<void> {
+async function dec01(): Promise<void> {
 	await vscode.commands.executeCommand('incrementor.decByTenth');
 }
+async function incByCustomValue(value: number): Promise<void> {
+	await vscode.commands.executeCommand('incrementor.incByCustomValue', value);
+}
+
 interface IIdk {
 	startOfTheDocument: [number, number, number, number];
 }
@@ -66,27 +71,27 @@ beforeEach(done => setTimeout(done, 100));
 describe('One cursor, simple examples', () => {
 	it('Increment 1️ should be 2', async () => {
 		await setDocumentText('1');
-		await runIncrement();
+		await inc();
 		const text = await getAllEditorText();
 		expect(text).to.equal('2');
 	});
 
 	it('Decrement 1 should be 0', async () => {
 		await setDocumentText('1');
-		await runDecrement();
+		await dec();
 		const text = await getAllEditorText();
 		expect(text).to.equal('0');
 	});
 
 	it('Increment 1 by 10  should be 11', async () => {
 		await setDocumentText('1');
-		await runIncrement10();
+		await inc10();
 		const text = await getAllEditorText();
 		expect(text).to.equal('11');
 	});
 	it('Decrement 0 by 10  should be -10', async () => {
 		await setDocumentText('0');
-		await runDecrement10();
+		await dec10();
 		const text = await getAllEditorText();
 		expect(text).to.equal('-10');
 	});
@@ -99,7 +104,7 @@ describe('One cursor, simple examples', () => {
 	// });
 	it('Decrement -20 by 0.1  should be -20.1', async () => {
 		await setDocumentText('-20');
-		await runDecrement01();
+		await dec01();
 		const text = await getAllEditorText();
 		expect(text).to.equal('-20.1');
 	});
@@ -108,14 +113,29 @@ describe('One cursor, simple examples', () => {
 describe('Numbers with text, like "1px"', () => {
 	it('Increment 0px should be 1px', async () => {
 		await setDocumentText('0px');
-		await runIncrement();
+		await inc();
 		const text = await getAllEditorText();
 		expect(text).to.equal('1px');
 	});
 	it('Decrement 0px should be -1px', async () => {
 		await setDocumentText('0px');
-		await runDecrement();
+		await dec();
 		const text = await getAllEditorText();
 		expect(text).to.equal('-1px');
+	});
+});
+
+describe('Custom values', () => {
+	it('Inc 10 by 12.5 => 22.5', async () => {
+		await setDocumentText('10');
+		await incByCustomValue(12.5);
+		const text = await getAllEditorText();
+		expect(text).to.equal('22.5');
+	});
+	it('Inc -12 by -500', async () => {
+		await setDocumentText('-12');
+		await incByCustomValue(-500);
+		const text = await getAllEditorText();
+		expect(text).to.equal('-512');
 	});
 });
